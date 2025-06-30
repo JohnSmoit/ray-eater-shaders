e697c20b3c9853080b9d866d47d282affac26fdb
JohnSmoit <ju.smoit@gmail.com>
2025-06-29 20:21:06 -0700

    Added expandable tokneization for OBJ file parsing

--- src/obj_loader.zig
+++ src/obj_loader.zig
@@ -33,6 +33,16 @@ pub const TokenType = enum(u8) {
 
 
 const Token = union(TokenType) {
+    const dir_map = std.StaticStringMap(DirectiveType).initComptime(.{
+        .{"v", .Vertex},
+        .{"vt", .TexCoord},
+        .{"vn", .Normal},
+        .{"f", .Face},
+        .{"o", .Name},
+        .{"usemtl", .UseMat},
+        .{"mtllib", .MatLib},
+    });
+
     Directive: DirectiveType,
     String: []const u8,
     FloatingPoint: f32,
@@ -40,7 +50,27 @@ const Token = union(TokenType) {
     EOF,
 
     pub fn eql(self: Token, other: Token) bool {
+        const t1 = std.meta.activeTag(self);
+        const t2 = std.meta.activeTag(other);
 
+        if (t1 != t2) return false;
+
+        return switch (self) {
+            .Directive => other.Directive == self.Directive,
+            .String => std.mem.eql(self.String, other.String),
+            .FloatingPoint => self.FloatingPoint == other.FloatingPoint,
+            .EOF => return t2 == .EOF,
+
+            .FaceDef => |faces| {
+                for (0..faces.len) |i| {
+                    if (faces[i] != other.FaceDef[i]) {
+                        return false;
+                    }
+                }
+
+                return  true;
+            },
+        };
     }
 
     pub fn getType(self: Token) TokenType {
@@ -51,6 +81,42 @@ const Token = union(TokenType) {
             .FaceDef => .FaceDef,
         };
     }
+
+    fn parseFaceDef(raw: []const u8) ?[3]u32 {
+        var split = std.mem.splitScalar(u8, raw, '/'); 
+        
+        var faces = [_]u32{0} ** 3;
+        var ind: usize = 0;
+        while (split.next()) |part| : (ind += 1) {
+            // 0 is a valid "null" value in this case since .obj face defs are
+            // 1 indexed because of course they are
+            faces[ind] = std.fmt.parseInt(u32, part, 0) catch 0; 
+        }
+
+        return faces;
+
+    }
+
+    pub fn fromRaw(raw: []const u8, allocator: Allocator) !Token {
+        if (dir_map.get(raw)) |v| {
+            return Token{.Directive = v};
+        }
+
+        const fp: ?f32 = std.fmt.parseFloat(f32, raw) catch null;
+        if (fp) |v| {
+            return Token{.FloatingPoint = v};
+        }
+
+        const face: ?[3]u32 = parseFaceDef(raw);
+        if (face) |f| {
+            return Token{.FaceDef = f};
+        }
+
+        const str = try allocator.alloc(u8, raw.len);
+        @memcpy(str, raw);
+
+        return Token{.String = str};
+    }
 };
 
 const TokenList = std.ArrayListUnmanaged(Token);
@@ -58,7 +124,25 @@ const TokenList = std.ArrayListUnmanaged(Token);
 const buf_size: usize = 1024;
 
 fn makeTokens(comptime values: anytype) []const Token {
+    comptime var tokens: []const Token = &.{};
 
+    inline for (values) |val| {
+        tokens = tokens ++ [_]Token{
+            switch(@TypeOf(val)) {
+                DirectiveType => Token{.Directive = val},
+
+                f32 => Token{.FloatingPoint = val},
+                
+                struct {u32, u32, u32} => Token{.FaceDef = [3]u32{val[0], val[1], val[2]}},
+
+                []const u8 => Token{.String = val},
+            }
+        };
+    }
+
+    tokens = tokens ++ [_]Token{.{.EOF}};
+
+    return tokens;
 }
 
 allocator: Allocator,
@@ -104,33 +188,97 @@ pub fn deinit(self: *Self) void {
     self.tokens.deinit(self.allocator);
 }
 
+const TokenizerState = struct {
+    buf: []u8,
+    buf_size: usize = 0,
+    pos: usize = 0,
+    file: std.fs.File,
 
-const delim = [_]u8{' ', '\n', '\r', '\t'};
+    /// ## Notes:
+    /// - A return value of null indicates the EOF token...
+    pub fn nextTokenRaw(self: *TokenizerState) ?[]u8 {
+        // Stop conditions: 
+        //  - Whitespace character encountered
+        //  - End of File encountered
+        //      - reach buf size position, re read, then buf size = 0
+        //
+        //  Special Case: 
+        //  - End of buffer in the middle of token
+        //      - Handle this by refilling the buffer at an offset and resetting the token indices
+        //  - Returned token slice is valid until the next token is retrieved, since the buffer
+        //    could refill then
+
+        // eat preceding whitespace
+        while (std.ascii.isWhitespace(self.buf[self.pos])) : (self.pos += 1) {
+            if (self.pos == self.buf_size) {
+                self.pos = 0;
+
+                self.buf_size = try self.file.read(self.buf);
+
+                if (self.buf_size == 0) {
+                    return null;
+                }
+            }
+        }
+
+        var sym_begin = self.pos;
+        var sym_end = self.pos;
+
+        while (!std.ascii.isWhitespace(self.buf[self.pos]) and self.buf_size != 0) : (sym_end += 1) {
+            if (self.pos == self.buf_size) {
+                const sym_len: u64 = @intCast(sym_end - sym_begin);
+                const fpos = try self.file.getPos();
+
+                try self.file.seekTo(fpos - sym_len);
+                self.buf_size = try self.file.read(self.buf);
+
+                sym_begin = 0;
+                sym_end = sym_len;
+
+                self.pos = 0;
+            }
+
+            self.pos += 1;
+        }
+
+        if (self.buf_size == 0) {
+            return null;
+        }
+
+
+    }
+};
+
 
 fn tokenize(self: *Self, reader: AnyReader) ObjLoadErrors!void {
-    if (self.tokens != null) return;
-    
-    // Do tokenization stuf
-    const buf = try self.allocator.alloc(u8, buf_size);
+
+    // Do tokenization stuff
+    const allocator = self.str_allocator.allocator();
+    const buf = try allocator.alloc(u8, buf_size);
     defer self.allocator.free(buf);
 
-    var buf_pos: usize = 0;
-    var valid_bytes: usize = try reader.read(buf);
+    var tokenizer = TokenizerState{
+        .buf = buf,
+        .reader = reader,
+    };
 
-    var sym_begin: usize = 0;
-    var sym_end: usize = 0;
+    while (tokenizer.nextTokenRaw()) |raw| {
+        const tok = try Token.fromRaw(raw, allocator);
+        try self.tokens.append(allocator, tok);
+    }
 
-    // For file
-        // For token
-    // (Buffer stuff should not be the controlling iterator here)
-
+    try self.tokens.append(allocator, .{.EOF});
 }
 
 
+const log = std.log.scoped(.obj_loader);
 
 fn validateAgainstTokens(self: *const Self, tokens: []const Token) !void {
-
+    for (self.tokens.items, 0..) |tok, index| {
+        if (!tok.eql(tokens[index])) return error.MismatchedToken;
+    }
 }
+
 
 test "valid file" {
     var test_arena = ArenaAllocator.init(std.heap.page_allocator);
